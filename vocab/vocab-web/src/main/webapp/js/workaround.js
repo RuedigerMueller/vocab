42de18490fa4cc5faf57417a11b9d2af64dd2b45
Ruediger <Ruediger@rudigers-imac.fritz.box>
2014-04-21 10:29:23 +0200

    Workaround for Chrome Added noty

---
+++ vocab/vocab-web/src/main/webapp/js/workaround.js
@@ -0,0 +1,12 @@
+if (!Document.prototype.createAttributeNS) {  
+  Document.prototype.createAttributeNS = function(namespaceURI, qualifiedName) {  
+   var dummy = this.createElement('dummy');  
+   dummy.setAttributeNS(namespaceURI, qualifiedName, '');  
+   var attr = dummy.attributes[0];  
+   dummy.removeAttributeNode(attr);  
+   return attr;  
+  };  
+}  
+if (!Element.prototype.setAttributeNodeNS) {  
+  Element.prototype.setAttributeNodeNS = Element.prototype.setAttributeNode;  
+}  
